2e5732c5f53139de1300c454338f9a311040a1fa
Jason Yates <yatesj@casinorama.com>
2015-07-24 15:42:04 -0400

    Updated Deps

--- src/leiningen/new/hiccomp/project.clj
+++ src/leiningen/new/hiccomp/project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.2"]
-                 [ring/ring-defaults "0.1.4"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
                  [ring-server "0.4.0"]
                  [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.8.13"]]
