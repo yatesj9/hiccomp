48f8327db94e99a0fd4425aa0cda667435659c55
Jason Yates <yatesj@casinorama.com>
2015-08-01 09:45:06 -0400

    Updated Deps, added Midje

--- src/leiningen/new/hiccomp/project.clj
+++ src/leiningen/new/hiccomp/project.clj
@@ -2,12 +2,14 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
                  [ring-server "0.4.0"]
-                 [hiccup "1.0.5"]]
-  :plugins [[lein-ring "0.8.13"]]
+                 [hiccup "1.0.5"]
+                 [midje "1.7.0"]]
+  :plugins [[lein-ring "0.8.13"]
+            [lein-midje "3.1.3"]]
   :ring {:handler {{ns-name}}.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
