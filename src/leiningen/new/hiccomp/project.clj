5eb5edf0c05db41c3ed5938ee418811dc40f8ced
Jason Yates <yatesj@casinorama.com>
2016-03-02 13:53:04 -0500

    Updated Deps

--- src/leiningen/new/hiccomp/project.clj
+++ src/leiningen/new/hiccomp/project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
                  [ring-server "0.4.0"]
