c4cc5995d2851701926e2da0c63b70060c140296
Jason Yates <yatesj9@gmail.com>
2018-10-17 15:15:32 -0400

    Updated packages

--- src/leiningen/new/hiccomp/project.clj
+++ src/leiningen/new/hiccomp/project.clj
@@ -1,17 +1,17 @@
 (defproject {{ns-name}} "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Hiccomp template"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]
-                 [ring-server "0.4.0"]
-                 [ring-middleware-format "0.7.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring-server "0.5.0"]
+                 [ring-middleware-format "0.7.2"]
                  [hiccup "1.0.5"]
-                 [midje "1.8.3"]]
-  :plugins [[lein-ring "0.9.7"]
-            [lein-midje "3.2"]]
+                 [midje "1.9.3"]]
+  :plugins [[lein-ring "0.12.4"]
+            [lein-midje "3.2.1"]]
   :ring {:handler {{ns-name}}.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]]}})
